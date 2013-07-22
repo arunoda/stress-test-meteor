c01d024cd87c0dbdca980dba112fd1f27cd4a5e0
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2013-07-22 11:22:02 +0530

    added complete stress testing suite

---
+++ chat_app/server/logs.js
@@ -0,0 +1,40 @@
+var usage = Meteor.require('usage');
+
+Meteor.startup(function() {
+  showStats(2000);
+});
+
+function showStats(interval) {
+  console.log('timestamp,cpu,memory,groups,users,messages');
+  return setInterval(function() {
+    usage.lookup(process.pid, function(err, result) {
+      if(err) {
+        console.log(err);
+        return;
+      }
+
+      var groupStats = getGroupStat();
+      var toPrint = [
+        Date.now(),
+        result.cpu, 
+        result.memory, 
+        groupStats.groups,
+        groupStats.users,
+        groupStats.messages
+      ];
+      console.log(toPrint.join(','));
+    });
+  }, interval)  
+}
+
+function getGroupStat() {
+  var totalUsers = 0;
+  var totalGroups = 0;
+  for(var key in groupStat) {
+    totalGroups++;
+    totalUsers += groupStat[key];
+  }
+
+  var result = {users: totalUsers, groups: totalGroups, messages: messageCount};
+  return result;
+}
---
+++ stress_test/mongo_usage.js
@@ -0,0 +1,11 @@
+var usage = require('usage');
+var PID = process.argv[2];
+var TIMEOUT = process.argv[3] || 2000;
+
+console.log('cpu,memory');
+setInterval(function() {
+  usage.lookup(PID, function(err, result) {
+    if(err) throw err;
+    console.log(result.cpu +  ',' + result.memory);
+  });
+}, TIMEOUT);
